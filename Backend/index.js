5cb6621570ff0f17c5a98fccc81e376c7aff0094
Deedado71 <pipegp1775@yahoo.com>
2022-10-25 22:01:57 -0500

    Agregar mondoose y coneccion de la base de datos

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,15 @@
 // Importar el modulo de expess
 // forma convencional nodejs
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 // otra forma de javascript
 // import express from "express"
 
 // Para establecer las rutas de express
 const router = express.Router();
 
+// Importar las variables de entorno
+require("dotenv").config({path:"var.env"})
 // Inicializar express
 var app = express();
 // -------------------------------------------------------------------------------------------------------------
@@ -27,14 +30,32 @@ var app = express();
 app.use(router);
 router.get("/mesaje",function(req,res){
     res.send("Mensaje con Metodo GET")
+
+    const name_db = "OmegaShopGroupU31G5"
+    const user = "dast"
+    const paswr = "dast123"
+    const uri = `mongodb+srv://${user}:${paswr}@omegapetshopu31g05.b2v1ve4.mongodb.net/?retryWrites=true&w=majority`
+    mongoose.connect(uri)
+        .then(function(){
+            console.log("Base de datos conectada")
+        })
+        .catch(function(e){console.log("Error:"+e)})
 });
 
 router.post("/mesaje",function(req,res){
     res.send("Mensaje con Metodo POST");
 });
+
+router.put("/mesaje",function(req,res){
+    res.send("Mensaje con Metodo PUT")
+});
+
+router.delete("/mesaje",function(req,res){
+    res.send("Mensaje con Metodo DELETE")
+});
 // -------------------------------------------------------------------------------------------------------------
 // Asignacion del puerto
-app.listen(4000);
+ app.listen(process.env.PORT);
 
 // Mensaje de activacion de servidor web
 console.log("Servidor ejecutandose desde: http://localhost:4000/");

671a10708f43196100ff30dfa4f1c6dc43fe3998
Deedado71 <pipegp1775@yahoo.com>
2022-10-31 20:48:16 -0500

    Subida de esqueleto del backend completo con dependencias

--- Backend/index.js
+++ Backend/index.js
@@ -2,17 +2,26 @@
 // Importar el modulo de expess
 // forma convencional nodejs
 const express = require("express");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 // otra forma de javascript
 // import express from "express"
 
+// Importar las variables de entorno
+require("dotenv").config({path:"var.env"})
 // Para establecer las rutas de express
 const router = express.Router();
 
-// Importar las variables de entorno
-require("dotenv").config({path:"var.env"})
+// Importar la ruta de la configuración con la base de datos
+const conectarDB = require('./config/cxn_db');
+
+// Importar los CORS
+const cors = require('cors');
+
 // Inicializar express
 var app = express();
+
+// Conectar con la BAse de Datos
+conectarDB();
 // -------------------------------------------------------------------------------------------------------------
 // Pruebas minimas de funcionamientp
 // app.use("/",function(req,res){
@@ -27,35 +36,35 @@ var app = express();
 // -------------------------------------------------------------------------------------------------------------
 // Para usar los metodos http
 // const router = express.Router(); => aagregada a la linea 10 antes de inicializar 
-app.use(router);
-router.get("/mesaje",function(req,res){
-    res.send("Mensaje con Metodo GET")
+// app.use(router);
+// router.get("/mesaje",function(req,res){
+//     res.send("Mensaje con Metodo GET")
 
-    const name_db = "OmegaShopGroupU31G5"
-    const user = "dast"
-    const paswr = "dast123"
-    const uri = `mongodb+srv://${user}:${paswr}@omegapetshopu31g05.b2v1ve4.mongodb.net/?retryWrites=true&w=majority`
-    mongoose.connect(uri)
-        .then(function(){
-            console.log("Base de datos conectada")
-        })
-        .catch(function(e){console.log("Error:"+e)})
-});
+//     const name_db = "OmegaShopGroupU31G5"
+//     const user = "dast"
+//     const paswr = "dast123"
+//     const uri = `mongodb+srv://${user}:${paswr}@omegapetshopu31g05.b2v1ve4.mongodb.net/?retryWrites=true&w=majority`
+//     mongoose.connect(uri)
+//         .then(function(){
+//             console.log("Base de datos conectada")
+//         })
+//         .catch(function(e){console.log("Error:"+e)})
+// });
 
-router.post("/mesaje",function(req,res){
-    res.send("Mensaje con Metodo POST");
-});
+// router.post("/mesaje",function(req,res){
+//     res.send("Mensaje con Metodo POST");
+// });
 
-router.put("/mesaje",function(req,res){
-    res.send("Mensaje con Metodo PUT")
-});
+// router.put("/mesaje",function(req,res){
+//     res.send("Mensaje con Metodo PUT")
+// });
 
-router.delete("/mesaje",function(req,res){
-    res.send("Mensaje con Metodo DELETE")
-});
+// router.delete("/mesaje",function(req,res){
+//     res.send("Mensaje con Metodo DELETE")
+// });
 // -------------------------------------------------------------------------------------------------------------
 // Asignacion del puerto
- app.listen(process.env.PORT);
+app.listen(process.env.PORT);
 
 // Mensaje de activacion de servidor web
 console.log("Servidor ejecutandose desde: http://localhost:4000/");

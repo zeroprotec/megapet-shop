e10d98c55c544f131a7a8b7c973223f5ed665ff3
David Gonzalez <115372183+Deedado71@users.noreply.github.com>
2022-11-15 21:21:24 -0500

    Update index.js
    en otra tambien la cague

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,25 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
 import { BrowserRouter as Router } from 'react-router-dom';
->>>>>>> 995ae5bd732e89311852b0c62a8d2e7286eb8dde
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
   //<React.StrictMode>
     <Router>
       <App />
     </Router>
   //</React.StrictMode>
->>>>>>> 995ae5bd732e89311852b0c62a8d2e7286eb8dde
 );
 
 // If you want to start measuring performance in your app, pass a function

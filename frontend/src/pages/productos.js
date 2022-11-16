a5ae9fb8f85a5a0916dce65a0f8bd985bf3d68a8
Deedado71 <pipegp1775@yahoo.com>
2022-11-16 10:40:52 -0500

    cambios del backend para correr el aplicativo

--- frontend/src/pages/productos.js
+++ frontend/src/pages/productos.js
@@ -4,7 +4,7 @@
 import { useState } from "react";
 import Menu from "../components/menu";
 
-const IndexPages = () => {
+const ProductosPages = () => {
 
     const [producto, setProducto] = useState([])
 

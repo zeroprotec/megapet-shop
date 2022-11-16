f8e9650395a299c1f49f6e583d61ea851191640d
David Gonzalez <115372183+Deedado71@users.noreply.github.com>
2022-11-15 21:20:38 -0500

    Update App.js
    En este tambien

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-=======
 //import logo from './logo.svg';
 import ProductosPages from './pages/productos';
 import UsuariosPages from './pages/usuarios';
@@ -9,27 +5,10 @@ import IndexPages from './pages/indexpages';
 import FacturasPages from './pages/facturas';
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
->>>>>>> 995ae5bd732e89311852b0c62a8d2e7286eb8dde
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <header >
       <img width="600px" src="https://mascotasbichos.vtexassets.com/assets/vtex.file-manager-graphql/images/bf33e587-8032-486c-ace9-7592fb559ed3___ae0fc26e750baf67036da5946ca23ed6.png" alt="logo" />
       </header>
@@ -43,9 +22,9 @@ function App() {
 
 
       </div>
->>>>>>> 995ae5bd732e89311852b0c62a8d2e7286eb8dde
     </div>
   );
 }
 
 export default App;
+
